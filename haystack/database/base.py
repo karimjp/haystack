f496a5d8f8cb7d07541caf1745d13770ddc5f486
Tanay Soni <tanaysoni12@gmail.com>
2020-08-05 16:09:05 +0200

    Fix type hints

--- haystack/database/base.py
+++ haystack/database/base.py
@@ -2,6 +2,8 @@ from abc import abstractmethod, ABC
 from typing import Any, Optional, Dict, List, Union
 from uuid import uuid4
 
+import numpy as np
+
 
 class Document:
     def __init__(self, text: str,
@@ -9,7 +11,7 @@ class Document:
                  query_score: Optional[float] = None,
                  question: Optional[str] = None,
                  meta: Dict[str, Any] = None,
-                 embedding: Optional[List[float]] = None):
+                 embedding: Optional[np.array] = None):
         """
         Object used to represent documents / passages in a standardized way within Haystack.
         For example, this is what the retriever will return from the DocumentStore,
